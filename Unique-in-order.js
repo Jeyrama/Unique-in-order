e07e120e6387918a319ec4f444e05f42ec250b6a
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-14 18:01:49 -0700

    Update Unique-in-order.js

--- Unique-in-order.js
+++ Unique-in-order.js
@@ -17,3 +17,16 @@ let uniqueInOrder=function(iterable){
 }
 
 // or
+
+function uniqueInOrder(it) {
+  let result = []
+  let last
+  
+  for (let i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push(last = it[i])
+    }
+  }
+  
+  return result
+}
